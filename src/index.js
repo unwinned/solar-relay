bbb436406a8cf4efda0c6a429c367515def087c6
unwinned <alo337619@gmail.com>
2026-06-15 15:38:47 +0300

    Wire fee estimation, telemetry, and CLI demo into entrypoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import { RpcPool } from './core/rpc-pool.js';
 import { endpoints } from './config.js';
+import { createFeeEstimator } from './fees/estimator.js';
+import { createOtelTelemetry } from './telemetry/otel.js';
 
-const pool = new RpcPool(endpoints);
+const telemetry = createOtelTelemetry();
+const pool = new RpcPool(endpoints, { telemetry });
 
 for (let i = 0; i < 4; i++) {
   const result = await pool.execute((rpc) => rpc.getLatestBlockhash().send());
   console.log(`call ${i}: blockhash =`, result.value.blockhash.slice(0, 8));
 }
+
+const getFee = createFeeEstimator(pool);
+console.log('Fee estimate:', await getFee());
 console.log(pool.getHealthSnapshot());
+
+await telemetry.shutdown();
